cf95921b3f84190d1a233259c0921ae5a97e1d83
Nam Doan <tuannam@gmail.com>
2022-07-30 13:40:24 +1000

    fix layout

--- data/www/js/app.js
+++ data/www/js/app.js
@@ -12,17 +12,25 @@ const loadMore = () => {
   let output = ""
 
   console.log(`offset = ${offset}`);
-  $.getJSON(`/cgi-bin/category.cgi?${category}/${offset}/15`, function(movies) {
-    offset = offset + movies.length;
-
+  $.getJSON(`/cgi-bin/category.cgi?${category}/${offset}/15`, function(movies) {  
     movies.forEach(
-      ({ article_code, article_title, article_image }) =>
+      ({ article_code, article_title, article_image }) => {       
+        if (offset % 5 == 0) {
+          output += `<div class="box">`
+        } 
         (output += `<div class="movie">
-                <div class="movie-image"> <span class="play"><span class="name">${article_title}</span></span> 
-                <a href="#"><img src="${article_image}" alt="" /></a> </div>
-                <div class="rating">               
-                  <span class="comments"></span> </div>
-              </div>`)
+                      <div class="movie-image"> 
+                        <span class="play"><span class="name">${article_title}</span></span> 
+                        <a href="#"><img src="${article_image}" alt="" /></a> 
+                      </div>
+                      <div class="rating"><span class="comments"></span> </div>
+                    </div>`)
+        offset = offset + 1;
+        if (offset % 5 == 0) {
+          output += `</div>`
+        }
+      }
+        
     )    
     box.innerHTML += output;
     busy = false;
@@ -43,17 +51,24 @@ const showMovies = (name, category, container, all = false) => {
   if (all) {
     limit = 15
   }
-  $.getJSON(`/cgi-bin/category.cgi?${category}/0/${limit}`, function(movies) {
-    offset = offset + movies.length;
-
+  $.getJSON(`/cgi-bin/category.cgi?${category}/0/${limit}`, function(movies) {    
     movies.forEach(
-      ({ article_code, article_title, article_image }) =>
+      ({ article_code, article_title, article_image }) => {
+        if (offset % 5 == 0) {
+          output += `<div class="box">`
+        }
         (output += `<div class="movie">
-                <div class="movie-image"> <span class="play"><span class="name">${article_title}</span></span> 
-                <a href="#"><img src="${article_image}" alt="" /></a> </div>
-                <div class="rating">               
-                  <span class="comments"></span> </div>
-              </div>`)
+                      <div class="movie-image"> <span class="play"><span class="name">${article_title}</span></span> 
+                      <a href="#"><img src="${article_image}" alt="" /></a> </div>
+                      <div class="rating">               
+                      <span class="comments"></span> </div>
+                    </div>`)
+        offset = offset + 1;       
+        if (offset % 5 == 0) {
+          output += `</div>`
+        }
+      }
+       
     )
     if (!all) {
       output += `<div class="cl">&nbsp;</div>`
@@ -67,17 +82,17 @@ let path = $(location).attr('search');
 if (path.startsWith('?phimle')) {
   category="phimle"
   box = document.querySelector('.container')
-  box.innerHTML = `<div class="box phim-le"></div>`
+  box.innerHTML = `<div class="phim-le"></div>`
   document.addEventListener("DOMContentLoaded", showMovies('Phim Lẻ', 'phimle', '.phim-le', true));
 } else if (path.startsWith('?phimbo')) {
   category="phimbo"
   box = document.querySelector('.container')
-  box.innerHTML = `<div class="box phim-bo"></div>`
+  box.innerHTML = `<div class="phim-bo"></div>`
   document.addEventListener("DOMContentLoaded", showMovies('Phim Bộ', 'phimbo', '.phim-bo', true));
 } else if (path.startsWith('?phimchieurap')) {
   category="phimchieurap"
   box = document.querySelector('.container')
-  box.innerHTML = `<div class="box phim-chieu-rap"></div>`
+  box.innerHTML = `<div class="phim-chieu-rap"></div>`
   document.addEventListener("DOMContentLoaded", showMovies('Phim Chiếu Rạp', 'phimchieurap', '.phim-chieu-rap', true));
 } else {
   document.addEventListener("DOMContentLoaded", showMovies('Phim Chiếu Rạp', 'phimchieurap', '.phim-chieu-rap'));
