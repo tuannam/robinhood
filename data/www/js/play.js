274cb4c24a49b797d4eaea396d87557d443bb361
Nam Doan <tuannam@gmail.com>
2022-08-05 17:03:18 +1000

    chromecast

--- data/www/js/play.js
+++ data/www/js/play.js
@@ -6,12 +6,14 @@ const playMovie = (url) => {
         console.log(`/cgi-bin/resolve.cgi?${encodeURIComponent(url)}`);
         getJSON(`/cgi-bin/resolve.cgi?${encodeURIComponent(url)}`, function(resp) {  
             videoSrc = resp.url;
-            console.log(resp)
-            jwplayer("player").setup({ 
-                "playlist": [{
-                    "file": `${resp.url}`
-                }]
-            });
+
+            console.log(resp.url)
+            let video = document.getElementById("player")
+            var source = document.createElement('source');
+            source.setAttribute('src', resp.url);
+            source.setAttribute('type', 'video/mp4');
+            video.appendChild(source);
+            video.play();
         });     
     } else {
         console.log('there');
