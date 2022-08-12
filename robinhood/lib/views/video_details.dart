60829a6d2afddcf8e4a83dc567336609de620582
Nam Doan <tuannam@gmail.com>
2022-08-12 21:46:03 +1000

    chapters

--- robinhood/lib/views/video_details.dart
+++ robinhood/lib/views/video_details.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../model/section.dart';
 import '../service/api.dart';
@@ -12,7 +14,7 @@ class VideoDetailsWidget extends StatefulWidget {
 }
 
 class _VideoDetailsState extends State<VideoDetailsWidget> {
-  final _font = const TextStyle(fontSize: 18.0);
+  final _contentFont = const TextStyle(fontSize: 16.0);
   final String videoId;
   MovieDetails? details;
 
@@ -35,7 +37,7 @@ class _VideoDetailsState extends State<VideoDetailsWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final row = Row(
+    final row1 = Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         mainAxisSize: MainAxisSize.max,
         children: <Widget>[
@@ -60,10 +62,59 @@ class _VideoDetailsState extends State<VideoDetailsWidget> {
             padding: const EdgeInsets.all(10),
             child: Text(
               Uri.decodeComponent(details?.content ?? ''),
-              style: _font,
+              style: _contentFont,
             ),
           )),
         ]);
-    return row;
+    final chapters = Container(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+      child: Wrap(
+          spacing: 5,
+          runSpacing: 5,
+          children: (details?.links ?? [])
+              .map((e) => ChapterButton(link: e))
+              .toList()),
+    );
+    final column = Column(
+      children: [row1, chapters],
+    );
+    return SingleChildScrollView(
+      child: column,
+    );
+  }
+}
+
+class ChapterButton extends StatefulWidget {
+  final MovieLink link;
+
+  const ChapterButton({Key? key, required this.link}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ChapterButtonState();
+}
+
+class _ChapterButtonState extends State<ChapterButton> {
+  final _chapterFont = const TextStyle(fontSize: 18.0);
+  bool isFocus = false;
+
+  void _onFocus(bool value) {
+    setState(() {
+      isFocus = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+        onFocusChange: _onFocus,
+        child: Container(
+          color: isFocus ? Colors.yellow : Colors.transparent,
+          child: TextButton(
+              onPressed: () {},
+              child: Text(
+                widget.link.name,
+                style: _chapterFont,
+              )),
+        ));
   }
 }
