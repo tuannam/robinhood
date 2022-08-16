af5f9ee43952cc09fa684fdca07a8490fe09892c
Nam Doan <tuannam@gmail.com>
2022-08-16 17:21:56 +1000

    split webview player

--- robinhood/lib/views/video_player.dart
+++ robinhood/lib/views/video_player.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:robinhood/service/api.dart';
 // import 'package:video_player/video_player.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:robinhood/views/webview_player.dart';
 
 class PlayerWidget extends StatefulWidget {
   final String link;
@@ -14,19 +14,7 @@ class PlayerWidget extends StatefulWidget {
 
 class _PlayerWidgetState extends State<PlayerWidget> {
   // VideoPlayerController? _controller;
-  InAppWebView? _webView;
-  final _options = InAppWebViewGroupOptions(
-      crossPlatform: InAppWebViewOptions(
-        useShouldOverrideUrlLoading: true,
-        mediaPlaybackRequiresUserGesture: false,
-      ),
-      android: AndroidInAppWebViewOptions(
-        useHybridComposition: true,
-      ),
-      ios: IOSInAppWebViewOptions(
-        allowsInlineMediaPlayback: true,
-      ));
-  var buttonsVisible = false;
+  WebViewWidget? _webView;
 
   @override
   void initState() {
@@ -39,12 +27,8 @@ class _PlayerWidgetState extends State<PlayerWidget> {
   }
 
   void play(String mediaUrl) {
-    final wv = InAppWebView(
-      initialUrlRequest: URLRequest(url: Uri.parse(mediaUrl)),
-      initialOptions: _options,
-    );
     setState(() {
-      _webView = wv;
+      _webView = WebViewWidget(url: mediaUrl);
     });
     // _controller = VideoPlayerController.network(mediaUrl)
     //   ..initialize().then((_) {
@@ -61,24 +45,7 @@ class _PlayerWidgetState extends State<PlayerWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(children: <Widget>[
-          _webView ?? Container(),
-          Opacity(
-            opacity: buttonsVisible ? 1 : 0,
-            child: FloatingActionButton(
-              autofocus: true,
-              onPressed: () {
-                setState(() {
-                  buttonsVisible = !buttonsVisible;
-                });
-                // Navigator.of(context).pop(true);
-              },
-              child: const Icon(Icons.pause),
-            ),
-          )
-        ]),
-      ),
+      body: Center(child: _webView ?? Container()),
     );
   }
 }
---
+++ robinhood/lib/views/webview_player.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class WebViewWidget extends StatelessWidget {
+  final String url;
+
+  WebViewWidget({Key? key, required this.url}) : super(key: key);
+  final _options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: InAppWebView(
+            initialUrlRequest: URLRequest(url: Uri.parse(url)),
+            initialOptions: _options),
+      ),
+    );
+  }
+}
